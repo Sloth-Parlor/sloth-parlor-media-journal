005d7fa77d41e86b5d26c95510b46cd9c860a005
Jason Makhlouta <jason.makhlouta@gmail.com>
2024-04-01 19:19:36 -0400

    feat: mj user data resolving middleware
    incl. refactor into an initial "Core" project

---
+++ src/SlothParlor.MediaJournal.Core/Extensions/MediaJournalServiceCollectionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SlothParlor.MediaJournal.Core.JournalUser;
+using SlothParlor.MediaJournal.WebApp;
+
+namespace SlothParlor.MediaJournal.Core.Extensions;
+
+public static class MediaJournalServiceCollectionExtensions
+{
+    public static IServiceCollection AddMediaJournalCore(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddDbContext<MediaJournalDbContext>((services, options) =>
+        {
+            var configuration = services.GetRequiredService<IConfiguration>();
+            var hostEnvironment = services.GetService<IHostEnvironment>();
+            
+            var appDbConnectionString = configuration.GetConnectionString(Constants.AppDbConnectionStringKey);
+            options.UseNpgsql(appDbConnectionString);
+
+            if (hostEnvironment is not null && hostEnvironment.IsDevelopment())
+            {
+                options
+                    .EnableDetailedErrors()
+                    .EnableSensitiveDataLogging();
+            }
+        });
+
+        serviceCollection.AddScoped<IUserService, UserService>();
+
+        return serviceCollection;
+    }
+}
