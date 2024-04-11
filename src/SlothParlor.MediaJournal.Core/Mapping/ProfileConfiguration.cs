2cc098d171d20bc9894680da95e16428fc6d8d8f
Jason Makhlouta <jason.makhlouta@gmail.com>
2024-04-11 12:16:57 -0400

    chore(refactor): factor out auto-mapper config

---
+++ src/SlothParlor.MediaJournal.Core/Mapping/ProfileConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace SlothParlor.MediaJournal.Core.Mapping;
+
+public static class ProfileConfiguration
+{
+    public static void ConfigureProfiles(IMapperConfigurationExpression mapperConfiguration)
+    {
+        mapperConfiguration.AddProfile<WatchGroupProfile>();
+        mapperConfiguration.AddProfile<WatchGroupParticipantProfile>();
+    }
+}
